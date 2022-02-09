6c18172d298a7479f5793205fb3e9adb7320e924
Francisco Grande <fgrande@mobydigital.com>
2022-02-09 12:37:51 -0300

    CRUD Technology

--- src/main/java/moby/testbackend/service/TechnologyService.java
+++ src/main/java/moby/testbackend/service/TechnologyService.java
@@ -1,21 +1,48 @@
 package moby.testbackend.service;
 
+import moby.testbackend.model.Candidate;
 import moby.testbackend.model.Technology;
 import moby.testbackend.repository.TechnologyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.Objects.isNull;
 
 @Service
 public class TechnologyService {
 
     private final TechnologyRepository technologyRepository;
+    private final CandidateForTechnologyService candidateForTechnologyService;
 
     @Autowired
-    public TechnologyService(TechnologyRepository technologyRepository){
+    public TechnologyService(TechnologyRepository technologyRepository, CandidateForTechnologyService candidateForTechnologyService){
         this.technologyRepository = technologyRepository;
+        this.candidateForTechnologyService = candidateForTechnologyService;
+    }
+
+    public Technology addTechnology(Technology technology) {
+        return technologyRepository.save(technology);
+    }
+
+    public Page<Technology> getAllTechnologies(Pageable pageable) {
+        return technologyRepository.findAll(pageable);
     }
 
     public Technology getTechnologyById(Integer idTechnology){
         return technologyRepository.findById(idTechnology).orElse(null);
+    }
+
+    public Technology updateTechnology(Technology technology) {
+        return technologyRepository.save(technology);
+    }
+
+    public void deleteTechnology(Integer idTechnology) {
+        Technology technology = getTechnologyById(idTechnology);
+        if(isNull(candidateForTechnologyService.getCandidatesForTechnologyByTechnology(technology)))
+            technologyRepository.deleteById(idTechnology);
     }
 }
