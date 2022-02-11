344b49d02912a156ce139d1095d60c5d61483769
Francisco Grande <fgrande@mobydigital.com>
2022-02-11 01:49:35 -0300

    Added LoginControllerTest

--- src/test/java/moby/testbackend/testUtils/UserTestUtils.java
+++ src/test/java/moby/testbackend/testUtils/UserTestUtils.java
@@ -1,6 +1,8 @@
 package moby.testbackend.testUtils;
 
 import moby.testbackend.model.User;
+import moby.testbackend.model.dto.LoginRequestDto;
+import moby.testbackend.model.dto.UserDto;
 
 public class UserTestUtils {
 
@@ -10,6 +12,20 @@ public class UserTestUtils {
                 .username("mobydigital")
                 .password("123")
                 .email("moby@mobydigital.com")
+                .build();
+    }
+
+    public static UserDto getUserDto() {
+        return UserDto.builder()
+                .id(1)
+                .username("mobydigital")
+                .build();
+    }
+
+    public static LoginRequestDto getLoginRequestDto() {
+        return LoginRequestDto.builder()
+                .username("mobydigital")
+                .password("123")
                 .build();
     }
 }
